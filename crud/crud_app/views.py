715a8da5bc819b8f9227d1168c09541e51d71dd7
parkseoungmin <psm401@hanyang.ac.kr>
2021-06-24 14:41:35 +0900

    foreignkey

--- crud/crud_app/views.py
+++ crud/crud_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 
-from .models import Blog
+from .models import Blog, Comment
 
 # Create your views here.
 
@@ -14,13 +14,16 @@ def main(request):
 
 def detail(request, id):
     detail_data = get_object_or_404(Blog, pk=id)
+    comments = Comment.objects.filter(blog_id = id)
     context = {
         'title' : detail_data.title,
         'writer' : detail_data.writer,
         'body' : detail_data.body,
         'pub_date' : detail_data.pub_date,
-        'id' : id
-    }
+        'id' : id,
+        'comments': comments,
+        'image':detail_data.image,
+        }
     return render(request, 'detail.html', context)
 
 def create_page(request):
@@ -31,6 +34,7 @@ def create(request):
     new_data.title = request.POST['title']
     new_data.writer = request.POST['writer']
     new_data.body = request.POST['body']
+    new_data.image = request.FILES['image']
     new_data.pub_date = timezone.now()
     new_data.save()
     return redirect('main')
@@ -42,6 +46,7 @@ def update_page(request, id):
         'title' : update_data.title,
         'writer' : update_data.writer,
         'body'  :update_data.body,
+        'image' : update_data.image,
     }
     return render(request, 'update.html', context)
 
@@ -51,6 +56,7 @@ def update(request, id):
     update_data.writer = request.POST['writer']
     update_data.body = request.POST['body']
     update_data.pub_date = timezone.now()
+    update_data.image = request.POST['image']
     update_data.save()
     return redirect('main')
 
@@ -58,3 +64,20 @@ def delete(request, id):
     delete_data = get_object_or_404(Blog, pk=id)
     delete_data.delete()
     return redirect('main')
+
+def create_comment(requeset, id):
+    new_comment = Comment()
+    blog_id = Blog.objects.get(pk=id)
+    new_comment.blog_id = blog_id
+    new_comment.user = requeset.POST['user']
+    new_comment.content = requeset.POST['content']
+    new_comment.pub_date = timezone.now()
+    new_comment.save()
+
+    return redirect('detail', id)
+
+def delete_comment(request, id, comment_id):
+    comment = get_object_or_404(Comment, pk = comment_id)
+    comment.delete()
+
+    return redirect('detail', id)
