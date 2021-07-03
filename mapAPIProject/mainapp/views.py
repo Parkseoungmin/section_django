7da13e49524659f3ece416f58958a19c1215f6ee
psm <psm401@hanyang.ac.kr>
2021-07-03 16:37:32 +0900

    API

---
+++ mapAPIProject/mainapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Cafe
+
+# Create your views here.
+
+def main(request):
+    cafes = Cafe.objects.all()
+    return render(request, 'main.html', {'cafes' : cafes})
+    
